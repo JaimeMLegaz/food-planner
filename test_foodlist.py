5013d272ed71b76f06aac23aaa8161ae337442d8
JaimeMLegaz <jaimemlegaz@gmail.com>
2020-11-23 06:24:57 +0100

    Preparing tests for FoodList class

---
+++ test_foodlist.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from foodlist import
+
+class TestFoodList(TestCase):
+    def test_add_food(self):
+        self.fail()
+
+    def test_save_food(self):
+        self.fail()
+
+    def test_load_food(self):
+        self.fail()
